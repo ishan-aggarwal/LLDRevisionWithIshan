499ce5212f284b406e4742ee8e69d7c177f712f7
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-12-14 01:01:10 +0530

    Q1. Class Constructors - 1 (Basic OOPs)

---
+++ lld-1/src/main/java/oop_2/problems/q1/Person.java
@@ -0,0 +1,11 @@
+package oop_2.problems.q1;
+
+public class Person {
+    private int age;
+    private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
