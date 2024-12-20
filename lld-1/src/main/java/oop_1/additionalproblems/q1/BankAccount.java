92f0153f6aa6fda507d9bbbeaedb90f906147443
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-12-20 22:13:39 +0530

    Create BankAccount class

---
+++ lld-1/src/main/java/oop_1/additionalproblems/q1/BankAccount.java
@@ -0,0 +1,15 @@
+package oop_1.additionalproblems.q1;
+
+public class BankAccount {
+    String accountNumber;
+    int balance;
+    double roi;
+
+    double getSimpleInterest(int time) {
+        return balance * roi * time / 100;
+    }
+
+    double getBalanceWithInterest(int time) {
+        return balance + getSimpleInterest(time);
+    }
+}
