5bf4136ab412a0e31fa7a003e1a670da2d7ce80b
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-12-20 22:48:34 +0530

    Media File Player (Assignment)

---
+++ lld-1/src/main/java/oop_3/additionalproblems/q6/VideoFile.java
@@ -0,0 +1,20 @@
+package oop_3.additionalproblems.q6;
+
+public class VideoFile extends MediaFile {
+    private final String duration;
+
+    public VideoFile(String title, String fileName, String duration) {
+        super(title, fileName);
+        this.duration = duration;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing video file: " + fileName);
+    }
+
+    @Override
+    public String getDetails() {
+        return super.getDetails() + ", Duration: " + duration;
+    }
+}
