06b584b1daa0ed38b905d1172e0d2955e7837790
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2024-12-21 09:31:14 +0530

    Array Index Out Bounds Exception

---
+++ lld-1/src/main/java/exceptions/problems/q6/ArrayIndexOutOfBounds.java
@@ -0,0 +1,91 @@
+package exceptions.problems.q6;
+
+public class ArrayIndexOutOfBounds {
+    public static int accessArrayElement(int[] array, int index) {
+        try {
+            return array[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index");
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        int[] array = {1, 2, 3, 4, 5};
+
+        int index = 2;
+        int result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 4;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 5;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 0;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = -2;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 3;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = -1;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 10;
+        result = accessArrayElement(array, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        int arr[] = {0, 5, 3, 8, 9, 10000, 98432, 98127, 1243834, 233};
+
+        index = 0;
+        result = accessArrayElement(arr, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 3;
+        result = accessArrayElement(arr, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 9;
+        result = accessArrayElement(arr, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+
+        index = 6;
+        result = accessArrayElement(arr, index);
+        if (result != -1) {
+            System.out.println("The value stored at index " + index + " is " + result);
+        }
+    }
+}
---
+++ lld-1/src/main/java/exceptions/problems/q6/README.md
@@ -0,0 +1,13 @@
+# Array Index Out Bounds Exception
+
+## Problem Statement
+
+Write a program to return the value present at a given index in an array.
+
+## Instructions
+
+1. Implement the `accessArrayElement` method inside the `ArrayIndexOutOfBounds` class.
+2. You need to return the value stored at a particular index.
+3. Use the try-catch block to implement the function and return the output.
+4. In case of any exception, print the message `Invalid Index` and return `-1`.
+5. The array will always have non-negative integers.
