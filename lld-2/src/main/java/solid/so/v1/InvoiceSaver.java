32914433310d5899b6bdf026f021318dc1881c5c
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-01-06 20:23:14 +0530

    SOLID - S and O Design principle using Invoice Management

---
+++ lld-2/src/main/java/solid/so/v1/InvoiceSaver.java
@@ -0,0 +1,35 @@
+package solid.so.v1;
+
+public class InvoiceSaver {
+
+    private final Invoice invoice;
+
+    public InvoiceSaver(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+//    public void saveInvoice() {
+//        System.out.println("Saving invoice to DB: " + this.invoice.totalPrice());
+//    }
+
+    // code should be closed for modification
+    public void saveInvoice(String saveType) {
+        if ("DB".equals(saveType)) {
+            System.out.println("Saving invoice to DB: " + this.invoice.totalPrice());
+        } else if("FILE".equals(saveType)) {
+            System.out.println("Saving invoice to file: " + this.invoice.totalPrice());
+        } else if ("MYSQL".equals(saveType)) {
+
+        }
+    }
+}
+
+// Now in this class we are saving invoice to Database.
+// Let's say in future requirement comes to save the invoice to file.
+// We need to change the code in this class. This is not good. It violates the Open Close Principle.
+// We should not make any changes in this class. We should create a new class for saving invoice to file.
+// So, we will create an interface called InvoiceSaver and move the saveInvoice() method to that interface.
+// Then both DBInvoiceSaver and FileInvoiceSaver will implement this interface.
+// and provide their own implementation of saveInvoice() method.
+// This way we will be able to save invoice to DB and file without changing the code in this class.
+// and it will help us to follow the Open Close Principle and code remains extensible.
---
+++ lld-2/src/main/java/solid/so/v2/InvoiceSaver.java
@@ -0,0 +1,6 @@
+package solid.so.v2;
+
+public interface InvoiceSaver {
+
+    void saveInvoice();
+}
