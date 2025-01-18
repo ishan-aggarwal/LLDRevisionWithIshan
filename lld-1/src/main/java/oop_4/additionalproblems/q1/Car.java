81f5c99dcd657f5c26dae60d6c3a1661c189f262
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-01-18 21:08:38 +0530

    Class car main method added for test case

--- lld-1/src/main/java/oop_4/additionalproblems/q1/Car.java
+++ lld-1/src/main/java/oop_4/additionalproblems/q1/Car.java
@@ -1,5 +1,9 @@
 package oop_4.additionalproblems.q1;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Car implements Comparable<Car> {
     private int Price;
     private int Speed;
@@ -7,5 +11,24 @@ public class Car implements Comparable<Car> {
     @Override
     public int compareTo(Car o) {
         return this.Price - o.Price;
+//        return Integer.compare(this.Price, o.Price);
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Car car = new Car();
+        Field PriceField = car.getClass().getDeclaredField("Price");
+        PriceField.setAccessible(true);
+        PriceField.set(car, 10);
+
+        Car other = new Car();
+        PriceField.set(other, 20);
+
+        // car.compareTo(other)
+        Method compareToMethod = car.getClass().getDeclaredMethod("compareTo", Car.class);
+        Object retVal = compareToMethod.invoke(car, other);
+        int actualRetVal = (int) retVal;
+
+        // assertEquals(-10, actualRetVal);
+        System.out.println(actualRetVal);
     }
 }
