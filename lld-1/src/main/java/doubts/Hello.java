91adae5370ea0fa2f5b962be45fe1ffbf57739de
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2026-04-27 09:47:53 +0530

    Changes for day - 2 class

--- lld-1/src/main/java/doubts/Hello.java
+++ lld-1/src/main/java/doubts/Hello.java
@@ -3,5 +3,6 @@ package doubts;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // we are making some new changes - class - 2
     }
 }
